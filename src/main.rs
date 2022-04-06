2a634e5edb21ad0b623e51f8ef24166d1b934c99
Dr Maxim Orlovsky <orlovsky@pandoracore.com>
2022-04-06 09:58:33 +0200

    Moving back to GTK4 and adding real app window

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,163 @@
-use gtk::prelude::GtkWindowExt;
-use gtk::{ApplicationWindow};
-use relm4::{AppUpdate, Model, RelmApp, Sender, Widgets};
-use gladis4::Gladis;
+use gtk::{
+    prelude::BuilderExtManual, prelude::ButtonExt, prelude::LabelExt, prelude::WidgetExt, Button,
+    Inhibit, Label, Window,
+};
+use relm::{connect, Relm, Update, Widget, WidgetTest};
+use relm_derive::Msg;
 
-#[derive(Default)]
-struct AppModel {
-    counter: u8,
+use gladis::Gladis;
+
+struct Model {
+    counter: i32,
 }
 
-enum AppMsg {
-    Increment,
+#[derive(Msg)]
+enum Msg {
     Decrement,
+    Increment,
+    Quit,
 }
 
-impl Model for AppModel {
-    type Msg = AppMsg;
-    type Widgets = AppWidgets;
-    type Components = ();
+// Create the structure that holds the widgets used in the view.
+#[derive(Clone, Gladis)]
+struct Widgets {
+    /*
+    counter_label: Label,
+    minus_button: Button,
+    plus_button: Button,
+     */
+    window: Window,
 }
 
-impl AppUpdate for AppModel {
-    fn update(&mut self, msg: AppMsg, _components: &(), _sender: Sender<AppMsg>) -> bool {
-        match msg {
-            AppMsg::Increment => {
-                self.counter = self.counter.wrapping_add(1);
+struct Win {
+    model: Model,
+    widgets: Widgets,
+}
+
+impl Update for Win {
+    // Specify the model used for this widget.
+    type Model = Model;
+    // Specify the model parameter used to init the model.
+    type ModelParam = ();
+    // Specify the type of the messages sent to the update function.
+    type Msg = Msg;
+
+    fn model(_: &Relm<Self>, _: ()) -> Model {
+        Model { counter: 0 }
+    }
+
+    fn update(&mut self, event: Msg) {
+        /*
+        let label = &self.widgets.counter_label;
+
+        match event {
+            Msg::Decrement => {
+                self.model.counter -= 1;
+                // Manually update the view.
+                label.set_text(&self.model.counter.to_string());
             }
-            AppMsg::Decrement => {
-                self.counter = self.counter.wrapping_sub(1);
+            Msg::Increment => {
+                self.model.counter += 1;
+                label.set_text(&self.model.counter.to_string());
             }
+            Msg::Quit => gtk::main_quit(),
         }
-        true
+         */
     }
 }
 
-#[derive(Gladis, Clone)]
-pub struct AppWidgets {
-    pub window: ApplicationWindow,
+impl Widget for Win {
+    // Specify the type of the root widget.
+    type Root = Window;
+
+    // Return the root widget.
+    fn root(&self) -> Self::Root {
+        self.widgets.window.clone()
+    }
+
+    fn view(relm: &Relm<Self>, model: Self::Model) -> Self {
+        let glade_src = include_str!("../res/wallet.glade");
+        let widgets = Widgets::from_string(glade_src).unwrap();
+
+        widgets.window.show();
+
+        /*
+        connect!(
+            relm,
+            widgets.plus_button,
+            connect_clicked(_),
+            Msg::Increment
+        );
+        connect!(
+            relm,
+            widgets.minus_button,
+            connect_clicked(_),
+            Msg::Decrement
+        );
+        connect!(
+            relm,
+            widgets.window,
+            connect_delete_event(_, _),
+            return (Some(Msg::Quit), Inhibit(false))
+        );
+         */
+
+        Win { model, widgets }
+    }
 }
 
-impl Widgets<AppModel, ()> for AppWidgets {
-    type Root = ApplicationWindow;
+impl WidgetTest for Win {
+    type Streams = ();
 
-    fn root_widget(&self) -> Self::Root {
-        self.window.clone()
-    }
+    fn get_streams(&self) -> Self::Streams {}
 
-    fn init_view(_model: &AppModel, _components: &(), _sender: Sender<AppMsg>) -> Self {
-        let ui_string = include_str!("view/wallet.glade");
-        let widgets = Self::from_string(ui_string).expect("broken wallet.glade");
+    type Widgets = Widgets;
 
-        widgets.window.present();
-
-        widgets
-    }
-
-    fn view(&mut self, _model: &AppModel, _sender: Sender<AppMsg>) {
-        // Update widgets
+    fn get_widgets(&self) -> Self::Widgets {
+        self.widgets.clone()
     }
 }
 
 fn main() {
-    let model = AppModel::default();
-    let app = RelmApp::new(model);
-    app.run();
+    Win::run(()).expect("Win::run failed");
+}
+
+#[cfg(test)]
+mod tests {
+    use gtk::prelude::LabelExt;
+
+    use gtk_test::assert_text;
+    use relm_test::click;
+
+    use crate::Win;
+
+    #[test]
+    #[ignore]
+    fn label_change() {
+        let (_component, _, widgets) = relm::init_test::<Win>(()).expect("init_test failed");
+        let plus_button = &widgets.plus_button;
+        let minus_button = &widgets.minus_button;
+        let label = &widgets.counter_label;
+
+        assert_text!(label, 0);
+        click(plus_button);
+        assert_text!(label, 1);
+        click(plus_button);
+        assert_text!(label, 2);
+        click(plus_button);
+        assert_text!(label, 3);
+        click(plus_button);
+        assert_text!(label, 4);
+
+        click(minus_button);
+        assert_text!(label, 3);
+        click(minus_button);
+        assert_text!(label, 2);
+        click(minus_button);
+        assert_text!(label, 1);
+        click(minus_button);
+        assert_text!(label, 0);
+        click(minus_button);
+        assert_text!(label, -1);
+    }
 }
