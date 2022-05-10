fe2a30ed1a1db0184f5bd723cfe2a174eec8f9ed
Dawid Ciężarkiewicz <dpc@dpc.pw>
2022-05-09 22:20:53 -0700

    Add nix flake support
    All Rust NixOS contributors (like me) will want it. This is a basic,
    working setup, using a new and promissing Nix library for building
    Rust: https://github.com/ipetkov/crane

---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  description = "MyCitadel Wallet app for Linux, Windows & MacOS desktop made with GTK+ ";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
+    crane.inputs.nixpkgs.follows = "nixpkgs";
+
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, naersk, nixpkgs, flake-utils, flake-compat, fenix, crane }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+      fenix-pkgs = fenix.packages.${system};
+      fenix-channel = (fenix-pkgs.stable);
+
+      craneLib = (crane.mkLib pkgs).overrideScope' (final: prev: {
+        cargo = fenix-channel.cargo;
+        rustc = fenix-channel.rustc;
+      });
+
+      commonArgs = {
+        src = ./.;
+        buildInputs = [
+          pkgs.pango
+          pkgs.atk
+          pkgs.gtk3
+        ];
+        nativeBuildInputs = [
+          pkgs.pkgconfig
+          fenix-channel.rustc
+        ];
+      };
+
+      cargoArtifacts = craneLib.buildDepsOnly (commonArgs // {
+        pname = "mycitadel-deps";
+      });
+
+      mycitadel = craneLib.buildPackage (commonArgs // {
+        pname = "mycitadel";
+      });
+
+    in {
+      defaultPackage = mycitadel;
+
+      devShell = pkgs.mkShell {
+        buildInputs = cargoArtifacts.buildInputs;
+        nativeBuildInputs = cargoArtifacts.nativeBuildInputs ++ [
+          fenix-pkgs.rust-analyzer
+          fenix-channel.rustfmt
+          fenix-channel.rustc
+          fenix-channel.cargo
+        ];
+        RUST_SRC_PATH = "${fenix-channel.rust-src}/lib/rustlib/src/rust/library";
+      };
+  });
+}
