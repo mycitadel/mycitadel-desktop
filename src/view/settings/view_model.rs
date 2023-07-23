f8510e238dfd031e8ae57050914a91c588381364
Maxim Orlovsky <orlovsky@pandoraprime.ch>
2023-07-23 10:45:35 +0200

    settings: fix electrum test reporting

--- src/view/settings/view_model.rs
+++ src/view/settings/view_model.rs
@@ -364,12 +364,14 @@ impl ViewModel {
         std::thread::spawn(move || {
             match ElectrumClient::from_config(&url, config).and_then(|client| client.ping()) {
                 Err(err) => {
+                    eprintln!("failure: {err}");
                     sender
                         .send(ElectrumMsg::Failure(err.to_string()))
-                        .expect("channel broken");
+                        .expect("channel is broken");
                 }
                 Ok(_) => {
-                    sender.send(ElectrumMsg::Ok).expect("channel broken");
+                    eprintln!("success");
+                    sender.send(ElectrumMsg::Ok).expect("channel is broken");
                 }
             }
         });
